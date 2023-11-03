f49b80e496d0cf93cf25452fed44254835f704b3
ariannagutirrzz <ariannavega2021@gmail.com>
2023-11-03 02:00:03 -0400

    required field added for new todos

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,5 @@
 import React from "react"
 import { createPortal } from "react-dom"
-import "../../styles/modal-styles.css"
-
 function Modal ({ children }) {
     return createPortal(
         <div className="modal-styles">
